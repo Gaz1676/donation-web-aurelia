f916bd11bf46e33e8bacfc1802c32c706e97a7ad
Gaz1676 <gazfleming@gmail.com>
2017-11-27 17:07:22 +0000

    Donation-Web-Aurelia

---
+++ src/viewmodels/report/report.js
@@ -0,0 +1,15 @@
+// renders list of donations
+
+import {inject} from 'aurelia-framework';
+import DonationService from '../../services/donation-service';
+
+@inject(DonationService)
+export class Report {
+
+  donations = [];
+
+  constructor(ds) {
+    this.donationService = ds;
+    this.donations = this.donationService.donations;
+  }
+}
